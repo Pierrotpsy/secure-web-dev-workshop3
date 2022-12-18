b3bb4a3b21df97171fb953e988bda35e082efe41
Pierrotpsy <67154936+Pierrotpsy@users.noreply.github.com>
2022-12-18 17:55:04 +0100

    Update AccessMiddleware.js

--- middlewares/AccessMiddleware.js
+++ middlewares/AccessMiddleware.js
@@ -2,9 +2,11 @@ const access = (allowedRoles)=>(req,res, next)=>{
     if(!req.user){
         return res.status(403).send()
     }
+
     if (allowedRoles.includes(req.user.role)) {
         return next()
     }
+    
     return res.status(403).send()
 }
 
